975322e1a0e1837b070b5c01ad3e5e767bfc755c
Karl Kowal <kowal.karl99@gmail.com>
2023-11-29 23:49:12 +0100

    Metoder och kommentarer
    Lagt till kommentarer och gjorde hanteringen av CSV filer och JSON till egna metoder. Även förbättrar felhanteringen vid fel filformat.

--- src/FileManagement.java
+++ src/FileManagement.java
@@ -1,27 +1,74 @@
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import javax.swing.*;
+
 
 public class FileManagement {
 
     private static ArrayList<String[]> arraySize;
+    private static String[] header;
+    private static String[][] rows;
 
     FileManagement(String URL) {
         arraySize = new ArrayList<>();
         try {
-            File file = new File(URL);
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-                String[] array = line.split(",");
-                arraySize.add(array);
+            File file = new File(URL);              //Inputar path stringen till en ny file.
+            String[] extension = URL.split("\\.");  //Föreslet regx av intelliJ.
+            if(Objects.equals(extension[1],"csv"))
+                CSV(file);
+            else if(Objects.equals(extension[1],"json"))
+                JSON(file);
+            else{
+               JOptionPane.showMessageDialog(null,"Filformatet du valde stöds inte av programmet!","Error!",JOptionPane.INFORMATION_MESSAGE,null);  //Felmeddelande ifall man försöker något filformat som inte är csv/json.
+               System.exit(0);
             }
-            scanner.close(); // Glöm inte att stänga skannern när du är klar med den.
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
+        List<String[]> filData = FileManagement.getArraySize();                 //Hämtar datan från filen som har blivit inläst och spara den i en List av string arrayer.
+        header = filData.get(0);                                                //Plockar ut det första värdet i Listan för det kommer vara headern.
+        rows = filData.stream().skip(1).toArray(String[][]::new);               //Tar data från List och streamar genom den och skippar det första värdet i den eftersom att det är min header och jag vill inte ha den 2 gånger och gör det till en 2D String array som jag sedan spara i 2D arrayn rows.
     }
-    static ArrayList<String[]> getArraySize() {
+
+    private void JSON(File file) throws Exception {                     //Måste finns en try/catch eller throw för att FileReader ska funka.
+        JSONParser parser = new JSONParser();
+        FileReader reader = new FileReader(file);
+        Object obj = parser.parse(reader);                                      //Parsar filen och spara den som ett Object (obj)
+        JSONArray jsonArray = (JSONArray) obj;
+
+        for (Object json : jsonArray) {                                  //Loopar igenom varje element i jsonArray
+            JSONObject jsonObject = (JSONObject) json;                   //Castar om det till ett JsonObjekt.
+            List<String> rowData = new ArrayList<>();                    //Lagrar värdena av varje nyckel i jsonObject. Vilket i detta fallet är A,B,C,D,E,F,G,H
+
+            for (Object key : jsonObject.keySet())                       //Loopar igenom varje nyckel i objektet.
+                rowData.add(jsonObject.get(key).toString());             //Lägger till värdet av varje nyckel i listan som skaped innan. (Påminner lite om HashMaps)
+            arraySize.add(rowData.toArray(new String[0]));               //Lägger till det i "main" ArrayListen.
+        }
+    }
+    private void CSV(File file) throws Exception{
+        Scanner scanner = new Scanner(file);                        //Scannar filen
+        while(scanner.hasNext()){                                   //Loopar så länge det finns saker att scanna.
+            String line = scanner.nextLine();                       //Gör varje line till en string.
+            String[] array = line.split(",");                       //Splitar den Stringen vi varje komma och sätter det i en array
+            arraySize.add(array);                                   //Lägger till Arrayn i en ArrayList.
+        }
+        scanner.close();                                            //Stänger scannern för att undvika memoryleaks.
+    }
+    private static ArrayList<String[]> getArraySize() {
         return arraySize;
+    }
+    static String[] getHeader(){
+        return header;
+    }
+    static String[][] getRows(){
+        return rows;
     }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class GUI {
@@ -11,7 +13,7 @@ public class GUI {
 
     GUI() {
         filePicker();
-        FileManagement fil = new FileManagement(URL);
+        FileManagement fil = new FileManagement(URL);   //Instansierar klassen FileManager och skickar med Stringen URL till konstruktorn.
         JFrame frame = new JFrame();
         panel = new JPanel();
         frame.setLayout(new BorderLayout());
@@ -21,24 +23,20 @@ public class GUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         center();
-        panel.add(new JScrollPane(table),BorderLayout.CENTER);
-        frame.add(panel,BorderLayout.CENTER);
+        panel.add(new JScrollPane(table),BorderLayout.CENTER);      //Lägger till JTablet och en JScrollPane så att man kan scrolla i tablet.
+        frame.add(panel,BorderLayout.CENTER);                       //Sätter layout center och varför jag skapade en till panel och inte satte det direkt på framen är för att kanske i framtiden vill ha något i south eller norht som att lägga till celler/någon form av save funktion.
         frame.setVisible(true);
     }
-
     void center() {
-        List<String[]> filData = FileManagement.getArraySize();
-        String[] header = filData.get(0);
-        String[][] rows = filData.stream().skip(1).toArray(String[][]::new);
-        DefaultTableModel model = new DefaultTableModel(rows, header);
+        DefaultTableModel model = new DefaultTableModel(FileManagement.getRows(), FileManagement.getHeader());          //Gör en DTM där jag förklarar hur mitt JTabel ska formateras.
         table = new JTable(model);
-        table.setAutoCreateRowSorter(true);
+        table.setAutoCreateRowSorter(true);                                     //Använder mig av ACRS så att användaren själv kan sortera datan hur han/hon vill väl inne i programmet.
     }
     void filePicker(){
-        JFileChooser j = new JFileChooser("src");
-        int accept = j.showOpenDialog(null);
+        JFileChooser j = new JFileChooser("src");                               //Öppnar JFC i src-mappen.
+        int accept = j.showOpenDialog(null);                                    //Sparar värdet av vad användaren trycker på i JFCn.
         if(accept == JFileChooser.APPROVE_OPTION){
-            setURL(j.getSelectedFile().getAbsolutePath());
+            setURL(j.getSelectedFile().getAbsolutePath());                      //Tar Stringen som inehåller pathen till filen och setter den till min URL String.
         }
         else{
             System.exit(0);
