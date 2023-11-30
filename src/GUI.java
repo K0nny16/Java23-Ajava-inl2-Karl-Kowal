d2c45a6a34aa710354af11da7cf26791462801b2
K0nny16 <kowal.karl99@gmail.com>
2023-11-30 13:07:05 +0100

    Påbörjat en save function

--- src/GUI.java
+++ src/GUI.java
@@ -5,13 +5,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI {
+    DefaultTableModel model;
     private JFrame frame;
     private JPanel panel;
     private static String URL;
 
     GUI() {
         filePicker();
-        FileManagement fil = new FileManagement(URL);               //Instansierar klassen FileManager och skickar med Stringen URL till konstruktorn.
+        new FileManagement(URL);
         frame = new JFrame();
         panel = new JPanel();
         frame.setLayout(new BorderLayout());
@@ -25,7 +26,7 @@ public class GUI {
         frame.setVisible(true);
     }
     private void center() {
-        DefaultTableModel model = new DefaultTableModel(FileManagement.getRows(), FileManagement.getHeader());          //Gör en DTM där jag förklarar hur mitt JTabel ska formateras.
+        model = new DefaultTableModel(FileManagement.getRows(), FileManagement.getHeader());          //Gör en DTM där jag förklarar hur mitt JTabel ska formateras.
         JTable table = new JTable(model);
         table.setAutoCreateRowSorter(true);                                     //Använder mig av ACRS så att användaren själv kan sortera datan hur han/hon vill väl inne i programmet.
         panel.add(new JScrollPane(table),BorderLayout.CENTER);      //Lägger till JTablet och en JScrollPane så att man kan scrolla i tablet.
@@ -37,7 +38,7 @@ public class GUI {
         save.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                save();
             }
         });
         frame.add(bottom,BorderLayout.SOUTH);
@@ -46,11 +47,19 @@ public class GUI {
     }
     private void filePicker(){
         JFileChooser j = new JFileChooser("src");                               //Öppnar JFC i src-mappen.
-        int accept = j.showOpenDialog(null);                                    //Sparar värdet av vad användaren trycker på i JFCn.
+        int accept = j.showOpenDialog(null);                                    //Sparar värdet av vad användaren trycker på i JFC:n.
         if(accept == JFileChooser.APPROVE_OPTION)
             setURL(j.getSelectedFile().getPath());                              //Tar Stringen som inehåller pathen till filen och setter den till min URL String.
         else
             System.exit(0);
+    }
+    private void save(){
+        Object[][] tableData = new Object[model.getRowCount()][model.getColumnCount()];
+        for(int i = 0; i < model.getRowCount(); i++){
+            for(int j = 0; j < model.getColumnCount(); i++){
+                tableData[i][j] = model.getValueAt(i,j);
+            }
+        }
     }
     private void setURL(String s){
         URL = s;
