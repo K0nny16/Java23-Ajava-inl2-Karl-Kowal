a41264ba839e32ea388c074d9fa084f702239b73
K0nny16 <kowal.karl99@gmail.com>
2023-11-30 10:16:47 +0100

    Förbättrad inkapsulering

--- src/GUI.java
+++ src/GUI.java
@@ -1,12 +1,14 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 public class GUI {
-
+    JFrame frame;
     private JTable table;
     private JPanel panel;
     private static String URL;
@@ -14,7 +16,7 @@ public class GUI {
     GUI() {
         filePicker();
         FileManagement fil = new FileManagement(URL);   //Instansierar klassen FileManager och skickar med Stringen URL till konstruktorn.
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         panel = new JPanel();
         frame.setLayout(new BorderLayout());
         panel.setLayout(new BorderLayout());
@@ -25,22 +27,40 @@ public class GUI {
         center();
         panel.add(new JScrollPane(table),BorderLayout.CENTER);      //Lägger till JTablet och en JScrollPane så att man kan scrolla i tablet.
         frame.add(panel,BorderLayout.CENTER);                       //Sätter layout center och varför jag skapade en panel till och inte satte det direkt på framen är för att kanske i framtiden vill ha något i south eller norht som att lägga till celler/någon form av save funktion.
+        bottom();
         frame.setVisible(true);
     }
-    void center() {
+    private void center() {
         DefaultTableModel model = new DefaultTableModel(FileManagement.getRows(), FileManagement.getHeader());          //Gör en DTM där jag förklarar hur mitt JTabel ska formateras.
         table = new JTable(model);
         table.setAutoCreateRowSorter(true);                                     //Använder mig av ACRS så att användaren själv kan sortera datan hur han/hon vill väl inne i programmet.
     }
-    void filePicker(){
+    private void bottom(){
+        JButton save = new JButton("Save");
+        JPanel bottom = new JPanel();
+        save.setFocusable(false);
+        save.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveFile();
+            }
+        });
+        frame.add(bottom,BorderLayout.SOUTH);
+        bottom.setLayout(new FlowLayout());
+        bottom.add(save);
+    }
+    private void filePicker(){
         JFileChooser j = new JFileChooser("src");                               //Öppnar JFC i src-mappen.
         int accept = j.showOpenDialog(null);                                    //Sparar värdet av vad användaren trycker på i JFCn.
         if(accept == JFileChooser.APPROVE_OPTION)
-            setURL(j.getSelectedFile().getAbsolutePath());                      //Tar Stringen som inehåller pathen till filen och setter den till min URL String.
+            setURL(j.getSelectedFile().getPath());                              //Tar Stringen som inehåller pathen till filen och setter den till min URL String.
         else
             System.exit(0);
     }
-    void setURL(String s){
+    private void saveFile(){
+
+    }
+    private void setURL(String s){
         URL = s;
     }
 }
